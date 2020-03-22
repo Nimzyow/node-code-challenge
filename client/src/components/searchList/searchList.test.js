9aeb12ca341913de30c29962d77c8a6831b4498e
Nimzyow <n_soufiani@hotmail.com>
2020-03-22 10:45:42 +0000

    renders something if list is present test and code done

--- client/src/components/searchList/searchList.test.js
+++ client/src/components/searchList/searchList.test.js
@@ -5,7 +5,22 @@ import { findByTestAttr } from "../../../test/TestUtils";
 import SearchList from "./searchList";
 
 describe("SearchList.js", () => {
-  const defaultProps = { list: null };
+  const defaultProps = {
+    list: [
+      {
+        geonameid: 1,
+        name: "hastings",
+        latitude: 1000,
+        longitude: 2000
+      },
+      {
+        geonameid: 2,
+        name: "something",
+        latitude: 2000,
+        longitude: 3000
+      }
+    ]
+  };
 
   const setup = (props = {}) => {
     const setupProps = { ...defaultProps, ...props };
@@ -13,8 +28,13 @@ describe("SearchList.js", () => {
   };
 
   test("renders nothing if list is empty", () => {
-    let wrapper = setup();
+    const wrapper = setup({ list: null });
     const component = findByTestAttr(wrapper, "search-list");
     expect(component.length).toBe(0);
+  });
+  test("renders something if list is present", () => {
+    const wrapper = setup();
+    const component = findByTestAttr(wrapper, "search-list");
+    expect(component.length).toBe(1);
   });
 });
