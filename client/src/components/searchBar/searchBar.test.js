6a292005754f8d7bd1e4c2f58722bd8e378182a1
Nimzyow <n_soufiani@hotmail.com>
2020-03-22 00:12:35 +0000

    tested for initial value of search bar

--- client/src/components/searchBar/searchBar.test.js
+++ client/src/components/searchBar/searchBar.test.js
@@ -24,4 +24,11 @@ describe("SearchBar", () => {
     const component = findByTestAttr(wrapper, "search-bar");
     expect(component.length).toBe(1);
   });
+
+  test("search bar will have initial value of hast", () => {
+    const wrapper = setup();
+    const component = findByTestAttr(wrapper, "search-bar");
+    console.log(component.props().value);
+    expect(component.props().value).toBe("hast");
+  });
 });
